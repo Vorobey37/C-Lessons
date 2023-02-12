561c68bf2b109a1476ce8f7c0f29dba6e43032f6
Abvorobey <abvorobey@gmail.com>
2023-02-12 23:48:27 +0300

    Приветствие

---
+++ If_Else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя:");
+string u_n = Console.ReadLine();
+if (u_n.ToLower() == "алексей")
+{
+    Console.WriteLine("Чё, как дела, Лёхич?");
+}
+else 
+{
+    Console.Write("Привет,");
+    Console.WriteLine(u_n);
+}
