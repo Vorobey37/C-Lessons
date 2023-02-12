6d14cda3c642d37485694efe4b25c9825149c532
Abvorobey <abvorobey@gmail.com>
2023-02-13 00:39:33 +0300

    Максимальное число

---
+++ Max_Giri/Program.cs
@@ -0,0 +1,33 @@
+﻿int a = new Random().Next(-100,100);
+int b = new Random().Next(-100,100);
+int c = new Random().Next(-100,100);
+int d = new Random().Next(-100,100);
+int e = new Random().Next(-100,100);
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+
+int max = a;
+
+if(b>max) 
+{
+max=b;
+}
+
+if(c>max) 
+{
+    max=c;
+}
+
+if(d>max)
+{
+max=d;
+}
+if(e>max)
+{
+max=e;
+}
+Console.Write("Максимальное число - это ");
+Console.WriteLine(max);
