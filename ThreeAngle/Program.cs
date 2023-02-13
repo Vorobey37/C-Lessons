6b8861a0bb2719a15bc07c809c246b48a618ae33
Abvorobey <abvorobey@gmail.com>
2023-02-13 19:57:02 +0300

    Триэнгл

---
+++ ThreeAngle/Program.cs
@@ -0,0 +1,46 @@
+﻿Console.Clear();
+//Console.SetCursorPosition (40,0);
+//Console.WriteLine("+");
+
+int xa=20, ya=1, 
+ xb=1, yb=12,
+ xc=40, yc=12;
+
+Console.SetCursorPosition (xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition (xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition (xc, yc);
+Console.WriteLine("+");
+
+int x=xa, y=ya;
+int count=0;
+while (count<10000)
+{
+    int z=new Random().Next (0,3);
+    if (z==0)
+    {
+        x=(x+xa)/2;
+        y=(y+ya)/2;
+    }
+    if (z==1)
+    {
+        x=(x+xb)/2;
+        y=(y+yb)/2;
+    }
+    if (z==2)
+    {
+        x=(x+xc)/2;
+        y=(y+yc)/2;
+    }
+    Console.SetCursorPosition (x, y);
+    Console.WriteLine("+");
+    count++;
+}
+
+
+
+
+
